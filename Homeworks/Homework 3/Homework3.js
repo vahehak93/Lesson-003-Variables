dc51f471a67083cb5f07a5fd02e25a854b6813b0
vahehak93 <vahehak@gmail.com>
2021-12-01 22:59:38 +0400

    Initial commit

---
+++ Homeworks/Homework 3/Homework3.js
@@ -0,0 +1,40 @@
+// Task 1.1 Declare two variables: admin and name.
+// Task 1.2 Write the string "John" to the name variable.
+// Task 1.3 Copy the value from the name variable to admin.
+// Task 1.4 Display the value admin.
+
+let name,admin;
+name = "John";
+admin = name
+console.log(admin)
+
+
+
+
+
+// Task 2 What will be the output?
+let i = 10;
+let n = i++ %5;
+console.log(i,n)
+// The output is i = 11, n = 0
+
+
+// Task 3 Swap 2 variables
+let a = 10;
+let b = 3;
+let c = a + b; 
+b = c - b;
+a = c - b;             
+
+console.log(a,b)
+
+
+// Task 4
+//Swap 2 variables without using 3rd one
+let x = 10;
+let y = 3;
+x = x + y;
+y = x - y;
+x = x - y;
+         
+console.log(x,y)
